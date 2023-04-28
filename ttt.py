2c8b25c9a575a1f6da82089116f493375cdcc51d
Sam Thorold <sam.thorold@gmail.com>
2023-04-28 19:44:47 +0100

    tic-tac-toe minimax - Board class

--- ttt.py
+++ ttt.py
@@ -2,8 +2,12 @@
 
 from __future__ import annotations
 from enum import Enum
+import logging
 import sys
 from typing import Iterator
+
+
+logger = logging.getLogger(__name__)
 
 
 class IllegalMove(Exception):
@@ -15,12 +19,17 @@ class Player(Enum):
     O = "o"
 
 
-Move = tuple[Player, int, int]
+Move = tuple[int, int]
 
 
 class Board:
     @classmethod
-    def from_string(cls, string: str) -> Board:
+    def from_string(
+        cls,
+        string: str,
+        player: Player = Player.O,
+        depth: int = 0,
+    ) -> Board:
         state = "." * 9 if string is None else string
         state = state.lower()
         b: list[list[Player | None]] = []
@@ -29,141 +38,134 @@ class Board:
             for c in range(3):
                 ch = state[r * 3 + c]
                 b[-1].append(Player(ch) if ch != "." else None)
-        return cls(b)
+        return cls(b, player=player, depth=depth)
 
-    def __init__(self, state: list[list[Player | None]]):
+    def __init__(
+        self,
+        state: list[list[Player | None]],
+        player: Player,
+        moves: list[Move] | None = None,
+        depth: int = 0,
+    ):
         self.board = state
+        self.player = player
+        self.moves: list[Move] = [] if moves is None else moves
+        self.depth = depth
+
+    def __gt__(self, other: Board | int) -> bool:
+        if isinstance(other, Board):
+            other = other.score()
+        return self.score() > other
+
+    def __lt__(self, other: Board | int) -> bool:
+        if isinstance(other, Board):
+            other = other.score()
+        return self.score() < other
 
     def string(self) -> str:
-        s = ""
+        s = f"{self.player} to play ...\n"
         for row in self.board:
             for val in row:
                 s += "." if val is None else val.value
             s += "\n"
+        s += f"{self.score()}\n"
         return s
 
     def move(self, move: Move) -> Board:
-        pl, rix, cix = move
+        rix, cix = move
         if self.board[rix][cix]:
-            raise IllegalMove(
-                f"row={rix}, col={cix} is occupied ({self.board[rix][cix]})"
-            )
+            msg = f"row={rix}, col={cix} is occupied ({self.board[rix][cix]})"
+            logger.error(msg)
+            raise IllegalMove(msg)
         next_board = [[v for v in row] for row in self.board]
-        next_board[rix][cix] = pl
-        return Board(next_board)
+        next_board[rix][cix] = self.player
+        logger.debug("%s %s %s", self.player, rix, cix)
+        return Board(
+            state=next_board,
+            player=self.next_player(),
+            moves=self.moves + [move],
+            depth=self.depth + 1,
+        )
 
     def score(self) -> int:
         for p in Player:
             sign = 1 if p == Player.X else -1
             if any(all(c == p for c in row) for row in self.board):
-                return sign * 1
+                return sign * (10 - self.depth)
             if any(
                 all(c == p for c in [self.board[i][j] for i in range(3)])
                 for j in range(3)
             ):
-                return sign * 1
+                return sign * (10 - self.depth)
             if all(
                 [self.board[0][0] == p, self.board[1][1] == p, self.board[2][2] == p]
             ):
-                return sign * 1
+                return sign * (10 - self.depth)
             if all(
                 [self.board[2][0] == p, self.board[1][1] == p, self.board[0][2] == p]
             ):
-                return sign * 1
+                return sign * (10 - self.depth)
         return 0
 
     def completed(self) -> bool:
         return all(all(row) for row in self.board)
 
+    def next_player(self) -> Player:
+        return Player.X if self.player == Player.O else Player.O
 
-def next_player(pl: Player) -> Player:
-    return Player.X if pl == Player.O else Player.X
 
-
-def children(
-    board: Board,
-    turn: Player,
-    depth: int = 0,
-) -> Iterator[tuple[Board, Move, int, int]]:
+def children(board: Board) -> Iterator[Board]:
     # if the board is full or the last player just won
-    current_score = board.score()
-    if current_score or board.completed():
+    if board.score() or board.completed():
         return
     for rix, row in enumerate(board.board):
         for cix, val in enumerate(row):
             if val:
                 continue
-            move = (turn, rix, cix)
-            next_board = board.move(move)
-            current_score = next_board.score()
-            yield next_board, move, depth, current_score
+            yield board.move((rix, cix))
 
 
-# def tree(
-#     board: Board,
-#     turn: Player,
-#     depth: int = 0,
-# ) -> Iterator[tuple[Board, Move, int, int]]:
-#     for child in children(board=board, turn=turn, depth=depth):
-#         yield child
-#         yield from tree(board=child[0], turn=next_player(turn), depth=depth + 1)
-
-
-def minimax(
-    board: Board, pl: Player, depth: int = 0
-) -> tuple[Board, Move, int, int]:
-    if board.completed():
-        return board, (pl, -1, -1), depth, 0
-
-    best_board = Board([[]])
-    best_move = (Player.X, -1, -1)
-    best_depth = -1
+def minimax(board: Board) -> Board:
+    if board.score() or board.completed():
+        return board
 
     # maximising player
-    if pl == Player.X:
-        best_score = -10
-        for child in children(board, pl, depth):
-            if child[-1]:
-                candidate = child
-            else:
-                candidate = minimax(child[0], next_player(pl), depth + 1)
-            if candidate[-1] > best_score:
-                *_, best_score = candidate
-                best_board, best_move, *_ = child
+    if board.player == Player.X:
+        best_board = -10
+        for child in children(board):
+            best_board = max(best_board, minimax(child))
 
     # minimising player
-    elif pl == Player.O:
-        best_score = 10
-        for child in children(board, pl, depth):
-            if child[-1]:
-                candidate = child
-            else:
-                candidate = minimax(child[0], next_player(pl), depth + 1)
-            if candidate[-1] < best_score:
-                # best_board, best_move, best_depth, best_score = candidate
-                *_, best_score = candidate
-                best_board, best_move, *_ = child
+    elif board.player == Player.O:
+        best_board = 10
+        for child in children(board):
+            best_board = min(best_board, minimax(child))
 
     else:
-        raise ValueError(f"Unknown player {pl}.")
+        raise ValueError(f"Unknown player {board.player}.")
 
-    return best_board, best_move, best_depth, best_score
+    if isinstance(best_board, int):
+        raise RuntimeError("Well, this is not ideal. Seems no solution was found.")
+
+    return best_board
 
 
 def main() -> int:
-    board = Board.from_string("." * 9)
+    logging.basicConfig(level=logging.WARNING)
+    board = Board.from_string("." * 9, Player.O)
     while True:
-        p = Player.O
         r, c = [int(m) for m in input("Move: ")]
-        board = board.move((p, r, c))
+        board = board.move((r, c))
+        logger.info("%s %s", board.next_player(), board.moves[-1])
         print(board.string())
         s = board.score()
         if s or board.completed():
             print(s)
             break
-        p = Player.X
-        _, (_, r, c), *_ = minimax(board, p)
-        board = board.move((p, r, c))
+        variation = minimax(board)
+        move = variation.moves[board.depth]
+        logger.info("%s %s", board.next_player(), move)
+        board = board.move(move)
         print(board.string())
         s = board.score()
         if s or board.completed():
@@ -173,9 +175,12 @@ def main() -> int:
 
 
 if __name__ == "__main__":
-    # board = new_board("x..oo....")
-    # print(score(board, "x"))
-    # print(string(board))
+    # board = Board.from_string(".........", Player.O)
+    # board = board.move((1, 1))
+    # print(board.string())
+    # variation = minimax(board)
+    # print(variation.string())
+    # print(variation.moves, board.depth)
 
     # move = minimax(board, "x")
     # print(move)
