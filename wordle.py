db5d8e16f9435068b997f64f342142ffe51dbe3e
Sam Thorold <sam.thorold@gmail.com>
2023-04-30 14:43:50 +0100

    general alphabeta - wordle - sensible combo of alphabeta and heuristics

--- wordle.py
+++ wordle.py
@@ -108,6 +108,7 @@ class Board:
         is_min: bool = False,
         is_max: bool = False,
     ):
+        words = update_words(words, guesses=moves, statuses=statuses)
         self.words = words
         self.moves = moves
         self.statuses = statuses
@@ -157,6 +158,10 @@ class Board:
 
     def move(self, move: Guess | GuessStatus) -> Board:
         if isinstance(move, Guess):
+            if move._guess not in self.words:
+                raise ValueError(
+                    f"Guess '{move._guess}' not in words, might struggle."
+                )
             moves = self.moves + [move]
             statuses = self.statuses
         else:
@@ -200,11 +205,22 @@ class Board:
     def children(self) -> Iterator[Board]:
         if self.is_terminal():
             return
-        for word in self.words:
+        words = sorted(self.words, key=lambda w: -len(set(w)))
+        for word in words:
             if self.is_maximising():
                 yield self.move(Guess(word))
             else:
                 yield self.move(evaluate(Guess(word), self.moves[-1]))
+
+    def lowest_scoring_move(self) -> Guess:
+        if not self.moves and "crate" in self.words:
+            return Guess("crate")
+        children = list(self.children())
+        ch = children[0]
+        for c in children[1:]:
+            if c.score() < ch.score():
+                ch = c
+        return ch.moves[-1]
 
 
 def present(aim: Guess, guess: Guess, guessc: str, i: int) -> Status:
@@ -234,6 +250,39 @@ def _score(gs: GuessStatus) -> int:
     return sum(SCORES[s] for s in gs)
 
 
+def update_words(
+    words: set[str],
+    guesses: list[Guess],
+    statuses: list[GuessStatus],
+) -> set[str]:
+    if not guesses:
+        return words
+
+    for guess, status in zip(guesses, statuses):
+        if status == CORRECT_GUESS:
+            return set([str(guess)])
+        else:
+            words = set([w for w in words if w != str(guess)])
+
+        for i, (c, s) in enumerate(zip(str(guess), str(status))):
+            match s:
+                case Status.CORRECT.value:
+                    words = set([w for w in words if w[i] == c])
+                case Status.PRESENT.value:
+                    words = set([w for w in words if c in w and w[i] != c])
+                case Status.MISSING.value:
+                    # But, I use missing when there are present characters
+                    # but too many of them
+                    # So, if the letter is present or correct anywhere
+                    # elsewhere skip this missing filter
+                    stats = [s for s, c_ in zip(str(status), str(guess)) if c_ == c]
+                    if any(s != Status.MISSING.value for s in stats):
+                        words = set([w for w in words if c in w and w[i] != c])
+                        continue
+                    words = set([w for w in words if c not in w])
+    return words
+
+
 def main(words: set[str], aim: str) -> None:
     if aim not in words:
         raise ValueError("Aim not in words, might struggle.")
@@ -245,11 +294,12 @@ def main(words: set[str], aim: str) -> None:
     )
 
     while True:
-        if board.moves:
+        if board.score() < 5:
+            move = board.lowest_scoring_move()
+            board = board.move(move)
+        else:
             variation = search.alphabeta(board, a=board.minimum(), b=board.maximum())
             board = board.move(variation.moves[len(board.moves)])
-        else:
-            board = board.move(Guess(next(w for w in words)))
         status = evaluate(Guess(aim), board.moves[-1])
         board = board.move(status)
         print(board)
@@ -276,7 +326,8 @@ if __name__ == "__main__":
         "noose",
         "maxim",
     ]
-    # with open("words-min.txt") as fh:
-    #     words = fh.read().split("\n")
+    with open("words.txt") as fh:
+        words = fh.read().split("\n")
 
-    main(set(words), "maxim")
+    # main(set(words), "burnt")
+    main(set(words), "fizzy")
